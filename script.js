92e41815bc7beac5cfb37658cbc797fac3660109
rishi8815 <0112cs211099@gmail.com>
2024-09-24 13:59:04 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,43 +1,69 @@
- var tl = gsap.timeline()
- tl.from('.line h1',{
-    y:150,
-    stagger:0.25,
-    duration:0.6,
-    delay:0.5,
- })
- tl.from('#line1-part1',{
-    opacity:0,
-    onStart:function(){
-        var h5timer = document.querySelector(' #line1-part1 h5')
-        var grow= 0
-       setInterval(()=>{
-           if(grow<100){
-               h5timer.innerHTML = grow++
-           }
-       else{
-           h5timer.innerHTML = grow
+const loader =()=>{
+    var tl = gsap.timeline()
+    tl.from('.line h1',{
+       y:150,
+       stagger:0.25,
+       duration:0.6,
+       delay:0.5,
+    })
+    tl.from('#line1-part1',{
+       opacity:0,
+       onStart:function(){
+           var h5timer = document.querySelector(' #line1-part1 h5')
+           var grow= 0
+          setInterval(()=>{
+              if(grow<100){
+                  h5timer.innerHTML = grow++
+              }
+          else{
+              h5timer.innerHTML = grow
+          }
+          },35)
        }
-       },35)
-    }
- })
- tl.to('.line h2',{
-    animationName: 'anime',
-    opacity:1
- })
-tl.to('#loader',{
-    opacity:0,
-    duration:0.4,
-    delay:4
- })
- tl.from('#page1',{
-     delay:0.2,
-     duration:0.5,
-    y:1600,
-    opacity:0,
-    ease:Power4
+    })
+    tl.to('.line h2',{
+       animationName: 'anime',
+       opacity:1
+    })
+   tl.to('#loader',{
+       opacity:0,
+       duration:0.4,
+       delay:0
+    })
+    tl.from('#page1',{
+        delay:0.2,
+        duration:0.6,
+       y:1600,
+       opacity:0,
+       ease:Power4
+       
+    })
+    tl.to('#loader',{
+       display:'none',
+    });
+    tl.from("#nav",{
+       opacity:0
+
+    })
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+        y:120,
+        stagger:0.2
+
+    })
     
- })
- tl.to('#loader',{
-    display:'none',
- })
- 
+}
+
+loader()
+
+const cursorAnimation =()=>{
+document.addEventListener('mousemove',(dets)=>{
+    
+  gsap.to('#crsr',{
+    left:dets.x,
+    top:dets.y
+  })
+})
+
+Shery.makeMagnet("#nav2 h4", { });
+}
+cursorAnimation()
